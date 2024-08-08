e976a2332fd70168e6ed45c8452763de5a5a1502
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-08-08 18:30:00 +0000

    test

--- src/js/index.js
+++ src/js/index.js
@@ -7,7 +7,7 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
-import TodoList from "./component/todoliste.jsx";
+import {TodoList} from "./component/todoliste.jsx";
 
 //render your react application
 ReactDOM.createRoot(document.getElementById('app')).render(<div><Home/><TodoListe/></div>);
