0f15a79063764cd59e4aa41d7bf2a752952d104e
BinahDaath <79816877+BinahDaath@users.noreply.github.com>
2024-08-08 18:50:27 +0000

    test

--- src/js/component/todoliste.jsx
+++ src/js/component/todoliste.jsx
@@ -9,57 +9,9 @@ export function TodoList() {
   ]);
   const [task, setTask] = useState("");
 
-  const handleChangeTask = (ev) => {
-    setTask(ev.target.value);
-  };
-
-  const handleEnter = (ev) => {
-    if (ev.key === "Enter" && task.trim() !== "") {
-      setTodos([task.trim(), ...todos]);
-      setTask("");
-    }
-  };
-
-  const handleDelete = (index) => {
-    const newTodos = todos.filter((todo, i) => i !== index);
-    setTodos(newTodos);
-  };
 
   return (
     <div >
-      <div className="text-9xl text-center text-red-400">todos</div>
-      <div className="flex flex-row justify-center">
-    <div className="size-2/3">
-      <input
-        className="border size-full"
-        value={task}
-        onChange={handleChangeTask}
-        onKeyDown={handleEnter}
-      />
-
-      <ul>
-        {todos.map((todo, index) => (
-          <li key={index} className="flex items-center justify-between">
-            {todo}{" "}
-            <button
-              onClick={() => handleDelete(index)}
-              className="px-1 bg-red-400 hover:bg-red-700 text-white text-xs rounded-lg"
-            >
-              x
-            </button>
-          </li>
-        ))}
-      </ul>
-
-      <div>
-        {todos.length > 0 ? (
-          <span>{todos.length} items left</span>
-        ) : (
-          <span>No tasks, add a task</span>
-        )}
-      </div>
-    </div>
-    </div>
     </div>
   );
 }
